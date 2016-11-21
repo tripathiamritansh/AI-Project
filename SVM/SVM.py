9376a4503937dc2dbf194dd1cba54e9816caacd5
Amritansh Tripathi <amritanshtripathi@gmail.com>
2016-11-20 20:55:41 -0500

    svm commit

--- SVM/SVM.py
+++ SVM/SVM.py
@@ -2,3 +2,8 @@ from sklearn import svm
 import csv
 import matplotlib as plt
 
+train_data=open('train.csv')
+reader= csv.reader(train_data,delimiter=",")
+data = list(reader)
+row_count =len(data)
+
