64d7d8f0d4d0bcec2cbe2ca7740d5df8dff2b1b9
Amritansh Tripathi <amritanshtripathi@gmail.com>
2016-12-10 15:47:49 -0500

    Deleted svm

--- SVM/SVM.py
+++
@@ -1,9 +0,0 @@
-from sklearn import svm
-import csv
-import matplotlib as plt
-
-train_data=open('train.csv')
-reader= csv.reader(train_data,delimiter=",")
-data = list(reader)
-row_count =len(data)
-
