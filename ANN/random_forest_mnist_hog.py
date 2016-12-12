5c4630383c079ed5f38a6e5d15a94d1892b53068
Bharat V <vaidhyanathanb@ccs.neu.edu>
2016-12-12 01:32:15 -0500

    hog implementation

---
+++ ANN/random_forest_mnist_hog.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+from sklearn import cross_validation
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn import datasets
+from sklearn import preprocessing
+from skimage.feature import hog
+from nolearn.dbn import DBN
+import timeit
+
+image_data = datasets.fetch_mldata('MNIST Original') # Get the MNIST dataset.
+
+basic_x = image_data.data
+basic_y = image_data.target # Separate images from their final classification. 
+
+min_max_scaler = preprocessing.MinMaxScaler() # Create the MinMax object.
+basic_x = min_max_scaler.fit_transform(basic_x.astype(float)) # Scale pixel intensities only.
+
+train_x, test_x, train_y, test_y = cross_validation.train_test_split(basic_x, basic_y, test_size = 0.2, random_state = 0) # Split training/test.
+
+hogFeatures_train = []
+for feature in train_x:
+	f = hog(feature.reshape((28, 28)), orientations=9, pixels_per_cell=(4, 4), cells_per_block=(2, 2), visualise=False)
+	hogFeatures_train.append(f)
+
+print len(hogFeatures_train)
+
+hogFeatures_test = []
+for feature in test_x:
+	f = hog(feature.reshape((28, 28)), orientations=9, pixels_per_cell=(4, 4), cells_per_block=(2, 2), visualise=False)
+	hogFeatures_test.append(f)
+
+clf_rf = RandomForestClassifier()
+clf_rf.fit(hogFeatures_train, train_y)
+y_pred_rf = clf_rf.predict(hogFeatures_test)
+acc_rf = accuracy_score(test_y, y_pred_rf)
+print "random forest accuracy: ",acc_rf
